c956ad7b7ee9896452d5a506b89e427593c64a99
Adam Gadbois <adamgadbois@outlook.com>
2022-06-16 15:38:43 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require ('express')
 const morgan = require ('morgan')
+const cors = require ('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 
 morgan.token('json', (req) => {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
@@ -92,7 +95,7 @@ app.post("/api/persons", (request,response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
